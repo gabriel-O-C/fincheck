9889eb8fc31624ea47c975d246f9999e0b9db96d
gabriel chaves <gabrieloliveirachaves754@gmail.com>
2023-07-23 16:31:26 -0300

    feat: add user creation

---
+++ src/users/users.service.ts
@@ -0,0 +1,23 @@
+import { ConflictException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { PrismaService } from 'src/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prismaService: PrismaService) { }
+
+  async create(createUserDto: CreateUserDto) {
+    const { name, email, password } = createUserDto
+    const emailTaken = await this.prismaService.user.findUnique({
+      where: { email }
+    })
+
+    if(emailTaken) {
+      throw new ConflictException('this e-mail is already in use.')
+    }
+    const user = await this.prismaService.user.create({
+      data: { name, email, password }
+    })
+    return user;
+  }
+}
